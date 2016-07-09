2ac9b70eafd4ab0bfc42a3e9551f72181c6dfa3c
victorpaul <victorpaul.war@gmail.com>
2016-07-10 00:02:02 +0300

    added api

--- js/findermodel.js
+++ js/findermodel.js
@@ -1,18 +1,30 @@
 /**
  * Created by Victor on 09.07.2016.
  */
+var API_ENDPOINT = "http://10.0.1.249/api/";
+var API_GET_WORKERS = API_ENDPOINT + "workers";
+var API_GET_TAGS = API_ENDPOINT + "tags";
+
+var finderModel = function MyViewModel() {
+    var self = this;
+
+    self.filterWorkers = ko.observableArray([]);
+    self.filterTags = ko.observableArray([]);
+    self.allTags = ko.observableArray([]);
+    self.allWorkers = ko.observableArray([]);
+
+    self.init = function(){
+        $.get(API_GET_TAGS,function(data){
+            self.allWorkers(data.workers);
+        });
+        $.get(API_GET_WORKERS,function(data){
+            self.allTags(data.tags);
+        });
+    };
+
+    self.init();
+};
+
 $(function(){
-    function MyViewModel() {
-        this.people = [
-            { name: 'Franklin', technology: 'html', caste: 'developer' },
-            { name: 'Mario', technology: 'css', caste: 'developer' },
-            { name: 'Eugene', technology: 'jquery', caste: 'developer' },
-            { name: 'Sasha', technology: 'PS', caste: 'designer' },
-            { name: 'Alina', technology: 'PS', caste: 'designer' },
-            { name: 'Pasha', technology: 'PS', caste: 'designer' },
-            { name: 'Larisa', technology: '?', caste: 'manager' },
-            { name: 'Ianna', technology: '?', caste: 'manager' }
-        ]
-    }
-    ko.applyBindings(new MyViewModel());
+    ko.applyBindings(finderModel);
 });
